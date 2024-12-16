e5af0f90d3756bb67475fb980759279bd895c07c
Siddharth Sharma <22bec117@iiitdmj.ac.in>
2024-12-16 14:17:34 +0530

    changed

--- reduxToolkitTodo/src/components/Todos.jsx
+++ reduxToolkitTodo/src/components/Todos.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeTodo } from "../features/todo/todoSlices";
-
+import { removeTodo, updateTodo } from "../features/todo/todoSlices";
+import { useState } from "react";
 function Todos() {
   const todos = useSelector((state) => state.todos);
   const dispatch = useDispatch();
-
+  const [isEdit, setIsEdit] = useState(false);
+  const [newText, setNewText] = useState("");
+  const [flag, setFlag] = useState(false);
   return (
     <>
       <div>Todos</div>
@@ -15,26 +17,72 @@ function Todos() {
             className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
             key={todo.id}
           >
-            <div className="text-white">{todo.text}</div>
-            <button
-              onClick={() => dispatch(removeTodo(todo.id))}
-              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+            <div className="text-white">
+              {" "}
+              <input
+                type="text"
+                className={`border outline-none w-full bg-transparent rounded-lg ${
+                  isEdit ? "border-white/10 px-2" : "border-transparent"
+                }`}
+                value={flag ? newText : todo.text}
+                onChange={(e) => setNewText(e.target.value)}
+                readOnly={!isEdit}
+              />
+            </div>
+            <div className="flex space-x-2 ml-aut">
+              <button
+                className="text-white bg-yellow-500 border-0 py-1 px-4 focus:outline-none hover:bg-yellow-600 rounded text-md flex left-3"
+                onClick={() => {
+                  if (isEdit) {
+                    dispatch(updateTodo({ id: todo.id, text: newText }));
+                    setIsEdit(false);
+                  } else {
+                    setIsEdit(true);
+                    setNewText(todo.text);
+                    setFlag(true);
+                  }
+                }}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.862 3.487a2.25 2.25 0 113.182 3.182L6.75 20.964l-4.227.94.94-4.227L16.862 3.487z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.015 6.336L17.664 4.985"
+                  />
+                </svg>
+              </button>
+              <button
+                onClick={() => dispatch(removeTodo(todo.id))}
+                className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </button>
+            </div>
           </li>
         ))}
       </ul>
--- reduxToolkitTodo/src/features/todo/todoSlices.js
+++ reduxToolkitTodo/src/features/todo/todoSlices.js
@@ -13,7 +13,14 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.text = text;
+      }
+    },
   },
 });
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 export default todoSlice.reducer;
