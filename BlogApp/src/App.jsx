a2b319c18e2171a3bcc3b2e69a6f614d3a06e1e2
Siddharth Sharma <22bec117@iiitdmj.ac.in>
2024-12-25 18:17:47 +0530

    Blog App

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -1,12 +1,15 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import "./App.css";
 import authService from "./appwrite/auth";
 import { login, logOut } from "./store/authSlices";
 import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
+
 function App() {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
+
   useEffect(() => {
     authService
       .getCurrentUser()
@@ -19,11 +22,14 @@ function App() {
       })
       .finally(() => setLoading(false));
   }, []);
+
   return !loading ? (
     <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
       <div className="w-full block">
         <Header />
-        <main> TODO:{/* <Outlet /> */}</main>
+        <main>
+          TODO: <Outlet />
+        </main>
         <Footer />
       </div>
     </div>
--- BlogApp/src/pages/Home.jsx
+++ BlogApp/src/pages/Home.jsx
@@ -22,20 +22,21 @@ function Home() {
         </Container>
       </div>
     );
+  } else {
+    return (
+      <div className="w-full py-8">
+        <Container>
+          <div className="flex flex-wrap">
+            {posts.map((post) => (
+              <div key={post.$id} className="p-2 w-1/4">
+                <PostCard {...post} />
+              </div>
+            ))}
+          </div>
+        </Container>
+      </div>
+    );
   }
-  return (
-    <div className="w-full py-8">
-      <Container>
-        <div className="flex flex-wrap">
-          {posts.map((post) => (
-            <div key={post.$id} className="p-2 w-1/4">
-              <PostCard {...post} />
-            </div>
-          ))}
-        </div>
-      </Container>
-    </div>
-  );
 }
 
 export default Home;
